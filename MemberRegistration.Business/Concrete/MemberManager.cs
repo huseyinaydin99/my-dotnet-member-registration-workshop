171249b36f7400b22670ebf82d4bf2d8395db354
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-10-25 14:24:03 +0300

    Mernis sistemi Adapter tasarım kalıbıyla entegre edildi. Mernis SOAP(Single Object Access Protocol) Web Servisi ile sisteme register olmaya çalışan kullanıcının kimlik bilgileri doğrulanır. Mernis bilgileri T.C. Devleti nüfus kaydından alır.

--- MemberRegistration.Business/Concrete/MemberManager.cs
+++ MemberRegistration.Business/Concrete/MemberManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MemberRegistration.Business.Abstract;
+using MemberRegistration.Business.ServiceAdapters;
 using MemberRegistration.Entities.Concrete;
 
 namespace MemberRegistration.Business.Concrete
@@ -11,14 +12,17 @@ namespace MemberRegistration.Business.Concrete
     public class MemberManager : IMemberService
     {
         private IMemberService _memberService;
-
-        public MemberManager(IMemberService memberService)
+        private IKpsService _kpsService;
+        public MemberManager(IMemberService memberService, IKpsService kpsService)
         {
             _memberService = memberService;
+            _kpsService = kpsService;
         }
 
         public void Add(Member member)
         {
+            if (!_kpsService.ValidateUser(member)) throw new Exception("Girilen kullanıcı geçerli değil!");
+                //return;
             _memberService.Add(member);
         }
     }
