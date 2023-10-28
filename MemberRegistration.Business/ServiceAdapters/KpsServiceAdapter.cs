50c4d7df52ef3c2ae801395e807272cbf414529b
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-10-28 13:04:57 +0300

    ConsoleUI kısmındaki app.config dosyasına service model kısmı eklendi ve hatalar giderildi. Mernis sistemi üzerinden kullanıcı var mı diye kontrol ediliyor. Eğer öyle bir nüfus kaydı kayıt varsa ki o zaman kişi gerçekte var olan bir kişidir. O zaman kayıt gerçekleşir. Eğer nüfus kaydı yoksa kayıt gerçekleşmez. Aynı şekilde veritabanında aynı kayıt var mı diye kontrol eder ve aynı kayıttan varsa kayıt gerçekleşmez. Bu kontrol işinide Validation Rules(doğrulama kuralları) ile yapar. Doğrulama kuralları Business(iş) katmanında yapılır. Önce kötü kod yazıp sonra iyileştirme yaptım. Çalışır durumda.

--- MemberRegistration.Business/ServiceAdapters/KpsServiceAdapter.cs
+++ MemberRegistration.Business/ServiceAdapters/KpsServiceAdapter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MemberRegistration.Business.KpsServiceReference;
+using MemberRegistration.Business.KpsServiceReference2;
 using MemberRegistration.Entities.Concrete;
 
 namespace MemberRegistration.Business.ServiceAdapters
--- MemberRegistration.Business/ValidationRules/FluentValidation/MemberValidator.cs
+++ MemberRegistration.Business/ValidationRules/FluentValidation/MemberValidator.cs
@@ -13,7 +13,7 @@ namespace MemberRegistration.Business.ValidationRules.FluentValidation
     {
         public MemberValidator()
         {
-            RuleFor(x => x.Id).NotEmpty();
+            //RuleFor(x => x.Id).NotEmpty();
             RuleFor(x => x.FirstName).NotEmpty();
             RuleFor(x => x.LastName).NotEmpty();
             RuleFor(x => x.Email).NotEmpty().EmailAddress();
